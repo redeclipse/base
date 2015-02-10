253a564b03cdb4fffc31c2049373e9f1ff63fd77
qreeves <qreeves@gmail.com>
2015-02-11 03:24:35 +1000

    last semaphore fixes [skip ci]

--- src/semaphore.sh
+++ src/semaphore.sh
@@ -5,17 +5,16 @@ case $1 in
         sudo cp -ruv /var/cache/apt ${SEMAPHORE_CACHE_DIR}/apt
         sudo rm -rfv /var/cache/apt
         sudo ln -sv ${SEMAPHORE_CACHE_DIR}/apt /var/cache/apt
+        sudo dpkg --add-architecture i386
+        sudo apt-get update
         ;;
     build)
-        sudo apt-get update
         sudo apt-get -fy install build-essential zlib1g-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
         make PLATFORM=linux64 PLATFORM_BIN=amd64 INSTDIR=${HOME}/build/${BRANCH_NAME}/linux/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || exit 1
         if [ "${BRANCH_NAME}" = "master" ]; then
             sudo apt-get -fy install binutils-mingw-w64 g++-mingw-w64
             make PLATFORM=crossmingw64 PLATFORM_BIN=amd64 INSTDIR=${HOME}/build/${BRANCH_NAME}/windows/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || exit 1
             make PLATFORM=crossmingw32 PLATFORM_BIN=x86 INSTDIR=${HOME}/build/${BRANCH_NAME}/windows/bin/x86 CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 -C src clean install || exit 1
-            sudo dpkg --add-architecture i386
-            sudo apt-get update
             sudo apt-get -fy remove zlib1g-dev libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libpng-dev
             sudo apt-get -fy autoremove
             sudo apt-get -fy install build-essential multiarch-support g++-multilib zlib1g-dev:i386 libsdl1.2-dev:i386 libsdl-mixer1.2-dev:i386 libsdl-image1.2-dev:i386 libpng-dev:i386
