e104931e745e5d6cea53d0528e2df55d46e8ada7
roy williams <bewe@here.there>
2015-02-11 22:00:39 -0500

    Show the previous value of a variable when it is changed.

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -3130,7 +3130,8 @@ namespace server
         ident *id = idents.access(cmdname);
         if(id && id->flags&IDF_SERVER)
         {
-            const char *name = &id->name[3], *val = NULL;
+            const char *name = &id->name[3], *val = NULL, *oldval = NULL;
+            bool needfreeoldval = false;
             int locked = max(id->flags&IDF_ADMIN ? PRIV_ADMINISTRATOR : 0, G(varslock));
             #ifndef STANDALONE
             if(servertype < 3 && (!strcmp(id->name, "sv_gamespeed") || !strcmp(id->name, "sv_gamepaused"))) locked = PRIV_ADMINISTRATOR;
@@ -3187,6 +3188,7 @@ namespace server
                         return;
                     }
                     checkvar(id, arg);
+                    oldval = intstr(id);
                     *id->storage.i = ret;
                     id->changed();
                     val = intstr(id);
@@ -3217,6 +3219,7 @@ namespace server
                         return;
                     }
                     checkvar(id, arg);
+                    oldval = floatstr(*id->storage.f);
                     *id->storage.f = ret;
                     id->changed();
                     val = floatstr(*id->storage.f);
@@ -3241,6 +3244,8 @@ namespace server
                         return;
                     }
                     checkvar(id, arg);
+                    oldval = newstring(*id->storage.s);
+                    needfreeoldval = true;
                     delete[] *id->storage.s;
                     *id->storage.s = newstring(arg);
                     id->changed();
@@ -3252,7 +3257,16 @@ namespace server
             if(val)
             {
                 sendf(-1, 1, "ri2sis", N_COMMAND, ci->clientnum, name, strlen(val), val);
-                relayf(3, "\fy%s set %s to %s", colourname(ci), name, val);
+                if(oldval)
+                {
+                  relayf(3, "\fy%s set %s to %s (was: %s)", colourname(ci), name, val, oldval);
+                  if(needfreeoldval)
+                    delete[] oldval;
+                }
+                else
+                {
+                  relayf(3, "\fy%s set %s to %s", colourname(ci), name, val);
+                }
             }
         }
         else srvmsgf(ci->clientnum, "\frunknown command: %s", cmd);
