9e8b7a63f476d3ded29b02cb15cf46ec44ce3920
qreeves <qreeves@gmail.com>
2015-02-13 14:30:43 +1000

    more update.bat improvements and fixes

--- bin/update.bat
+++ bin/update.bat
@@ -48,8 +48,9 @@ if NOT EXIST "%REDECLIPSE_TMP%" mkdir "%REDECLIPSE_TMP%"
 echo Querying version from: %REDECLIPSE_TMP%\branch.txt
 if EXIST "%REDECLIPSE_TMP%\branch.txt" set /p REDECLIPSE_VERSION=< "%REDECLIPSE_TMP%\branch.txt"
 if "%REDECLIPSE_VERSION%" == "" set REDECLIPSE_VERSION=0
+set REDECLIPSE_VERSION=%REDECLIPSE_VERSION:~0,14%
 echo.
-echo Current version: %REDECLIPSE_UPDATE% %REDECLIPSE_VERSION%
+echo Cached version: %REDECLIPSE_UPDATE% %REDECLIPSE_VERSION%
 echo Fetching version information from: %REDECLIPSE_SOURCE%/%REDECLIPSE_UPDATE%/version.txt
 echo.
 if EXIST "%REDECLIPSE_TMP%\version.txt" del /f /q "%REDECLIPSE_TMP%\version.txt"
@@ -59,14 +60,15 @@ if NOT EXIST "%REDECLIPSE_TMP%\version.txt" (
     echo.
     goto bail
 )
-set /p REDECLIPSE_RETURN=< "%REDECLIPSE_TMP%\version.txt"
-if "%REDECLIPSE_RETURN%" == "" (
+set /p REDECLIPSE_REMOTE=< "%REDECLIPSE_TMP%\version.txt"
+if "%REDECLIPSE_REMOTE%" == "" (
     echo Failed to retrieve version update information. Trying to run anyway..
     echo.
     goto bail
 )
-if NOT "%REDECLIPSE_TRYUPDATE%" == "1" if %REDECLIPSE_RETURN% LEQ %REDECLIPSE_VERSION% goto good
-echo Updated version: %REDECLIPSE_BRANCH% %REDECLIPSE_RETURN%
+set REDECLIPSE_REMOTE=%REDECLIPSE_REMOTE:~0,14%
+echo Remote version: %REDECLIPSE_BRANCH% %REDECLIPSE_REMOTE%
+if NOT "%REDECLIPSE_TRYUPDATE%" == "1" if "%REDECLIPSE_REMOTE%" == "%REDECLIPSE_VERSION%" goto good
 echo Downloading update from: %REDECLIPSE_SOURCE%/%REDECLIPSE_UPDATE%/windows.zip
 echo.
 if EXIST "%REDECLIPSE_TMP%\windows.zip" del /f /q "%REDECLIPSE_TMP%\windows.zip"
@@ -78,6 +80,7 @@ if NOT EXIST "%REDECLIPSE_TMP%\windows.zip" (
     goto bail
 )
 echo Deploying to: %REDECLIPSE_PATH%
+echo "%REDECLIPSE_PATH%\bin\tools\unzip.exe" -o "%REDECLIPSE_TMP%\windows.zip" -d "%REDECLIPSE_PATH%" ^|^| exit /b 1 > "%REDECLIPSE_TMP%\install.bat"
 copy /y nul test.tmp > nul 2>&1 && (
     del /f /q test.tmp
     goto unpack
@@ -88,21 +91,21 @@ if NOT EXIST bin\tools\elevate.exe (
     echo.
     goto unpack
 )
-bin\tools\elevate.exe -wait "%REDECLIPSE_PATH%\bin\tools\unzip.exe" -o """%REDECLIPSE_TMP%\windows.zip""" -d """%REDECLIPSE_PATH%""" || (
+bin\tools\elevate.exe -wait "%REDECLIPSE_TMP%\install.bat" || (
     echo There was an error deploying the files. Trying to run anyway...
     echo.
     goto bail
 )
 goto branch
 :unpack
-"%REDECLIPSE_PATH%\bin\tools\unzip.exe" -o "%REDECLIPSE_TMP%\windows.zip" -d "%REDECLIPSE_PATH%" || (
+call "%REDECLIPSE_TMP%\install.bat" || (
     echo There was an error deploying the files. Trying to run anyway...
     echo.
     goto bail
 )
 echo.
 :branch
-echo %REDECLIPSE_RETURN% > "%REDECLIPSE_TMP%\branch.txt"
+echo %REDECLIPSE_REMOTE% > "%REDECLIPSE_TMP%\branch.txt"
 :good
 echo Everything is up to date!
 :bail
