29b59d47ff11e82f95340bd93625a64028f3ab83
lsalzman <lsalzman@gmail.com>
2015-02-14 07:27:52 -0800

    vectoyawpitch fix

--- src/shared/geom.cpp
+++ src/shared/geom.cpp
@@ -28,8 +28,12 @@ void vecfromyawpitch(float yaw, float pitch, int move, int strafe, vec &m)
 
 void vectoyawpitch(const vec &v, float &yaw, float &pitch)
 {
-    yaw = -atan2(v.x, v.y)/RAD;
-    pitch = asin(v.z/v.magnitude())/RAD;
+    if(v.iszero()) yaw = pitch = 0;
+    else
+    {
+        yaw = -atan2(v.x, v.y)/RAD;
+        pitch = asin(v.z/v.magnitude())/RAD;
+    }
 }
 
 static inline float det2x2(float a, float b, float c, float d) { return a*d - b*c; }
