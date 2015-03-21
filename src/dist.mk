52e55329b7055241debd4be290b4e10cf34566e5
qreeves <qreeves@gmail.com>
2015-03-21 23:22:10 +1000

    escape directory for temporary space

--- src/dist.mk
+++ src/dist.mk
@@ -22,12 +22,12 @@ endif
 
 DISTFILES=$(shell cd ../ && find . -not -iname *.lo -not -iname *.gch -not -iname *.o || echo "")
 
-../$(dirname):
+../../$(dirname):
 	rm -rf $@
 	# exclude VCS and transform relative to src/ dir
 	tar --exclude='.svn' --exclude='*.git' --exclude='*.hg' \
 		--exclude='*.bzr' \
-		-cf - $(DISTFILES:%=../%) | (mkdir $@/; cd $@/ ; tar -xpf -)
+		-cf - $(DISTFILES:%=../../%) | (mkdir $@/; cd $@/ ; tar -xpf -)
 	$(MAKE) -C $@/src clean
 	$(MAKE) -C $@/src/enet clean
 	echo "stable" > $@/branch.txt
@@ -41,106 +41,106 @@ DISTFILES=$(shell cd ../ && find . -not -iname *.lo -not -iname *.gch -not -inam
 	unzip -o windows.zip -d $@
 	rm -f windows.zip
 
-distdir: ../$(dirname)
+distdir: ../../$(dirname)
 
-../$(tarname): ../$(dirname)
+../../$(tarname): ../../$(dirname)
 	tar \
 		--exclude='$</bin/*/*.exe' \
 		-cf $@ $<
 
-dist-tar: ../$(tarname)
+dist-tar: ../../$(tarname)
 
-../$(tarname-all): ../$(dirname)
+../../$(tarname-all): ../../$(dirname)
 	tar -cf $@ $<
 
-dist-tar-all: ../$(tarname-all)
+dist-tar-all: ../../$(tarname-all)
 
-../$(tarname-osx): ../$(dirname)
+../../$(tarname-osx): ../../$(dirname)
 	tar -cf $@ -C $</bin $(dirname-osx)
 	mkdir tmpdir-osx
 	mkdir tmpdir-osx/$(dirname-osx)
 	mkdir tmpdir-osx/$(dirname-osx)/Contents
 	mkdir tmpdir-osx/$(dirname-osx)/Contents/Resources
 	# Use links with tar dereference to change directory paths
-	ln -s ../../$</data/ tmpdir-osx/$(dirname-osx)/data
-	ln -s ../../$</doc/ tmpdir-osx/$(dirname-osx)/doc
-	ln -s ../../$</src/ tmpdir-osx/$(dirname-osx)/src
-	ln -s ../../$</readme.txt tmpdir-osx/$(dirname-osx)/readme.txt
+	ln -s ../../../$</data/ tmpdir-osx/$(dirname-osx)/data
+	ln -s ../../../$</doc/ tmpdir-osx/$(dirname-osx)/doc
+	ln -s ../../../$</src/ tmpdir-osx/$(dirname-osx)/src
+	ln -s ../../../$</readme.txt tmpdir-osx/$(dirname-osx)/readme.txt
 	tar \
 		-hrf $@ -C tmpdir-osx $(dirname-osx)
 	rm -rf tmpdir-osx/
 
-dist-tar-osx: ../$(tarname-osx)
+dist-tar-osx: ../../$(tarname-osx)
 
-../$(dirname-win): ../$(dirname)
+../../$(dirname-win): ../../$(dirname)
 	cp -r $< $@
 	rm -rf $@/bin/*/*linux*
 	rm -rf $@/bin/*/*bsd*
 
-distdir-win: ../$(dirname-win)
+distdir-win: ../../$(dirname-win)
 
-../$(tarname).gz: ../$(tarname)
+../../$(tarname).gz: ../../$(tarname)
 	gzip -c < $< > $@
 
-dist-gz: ../$(tarname).gz
+dist-gz: ../../$(tarname).gz
 
-../$(tarname).bz2: ../$(tarname)
+../../$(tarname).bz2: ../../$(tarname)
 	bzip2 -c < $< > $@
 
-dist-bz2: ../$(tarname).bz2
+dist-bz2: ../../$(tarname).bz2
 
-dist-nix: ../$(tarname).bz2
+dist-nix: ../../$(tarname).bz2
 
-../$(tarname).xz: ../$(tarname)
+../../$(tarname).xz: ../../$(tarname)
 	xz -c < $< > $@
 
-dist-xz: ../$(tarname).xz
+dist-xz: ../../$(tarname).xz
 
-../$(tarname-all).gz: ../$(tarname-all)
+../../$(tarname-all).gz: ../../$(tarname-all)
 	gzip -c < $< > $@
 
-dist-gz-all: ../$(tarname-all).gz
+dist-gz-all: ../../$(tarname-all).gz
 
-../$(tarname-all).bz2: ../$(tarname-all)
+../../$(tarname-all).bz2: ../../$(tarname-all)
 	bzip2 -c < $< > $@
 
-dist-bz2-all: ../$(tarname-all).bz2
+dist-bz2-all: ../../$(tarname-all).bz2
 
-dist-all: ../$(tarname-all).bz2
+dist-all: ../../$(tarname-all).bz2
 
-../$(tarname-all).xz: ../$(tarname-all)
+../../$(tarname-all).xz: ../../$(tarname-all)
 	xz -c < $< > $@
 
-dist-xz-all: ../$(tarname-all).xz
+dist-xz-all: ../../$(tarname-all).xz
 
-../$(tarname-osx).gz: ../$(tarname-osx)
+../../$(tarname-osx).gz: ../../$(tarname-osx)
 	gzip -c < $< > $@
 
-dist-gz-osx: ../$(tarname-osx).gz
+dist-gz-osx: ../../$(tarname-osx).gz
 
-../$(tarname-osx).bz2: ../$(tarname-osx)
+../../$(tarname-osx).bz2: ../../$(tarname-osx)
 	bzip2 -c < $< > $@
 
-dist-bz2-osx: ../$(tarname-osx).bz2
+dist-bz2-osx: ../../$(tarname-osx).bz2
 
-dist-osx: ../$(tarname-osx).bz2
+dist-osx: ../../$(tarname-osx).bz2
 
-../$(tarname-osx).xz: ../$(tarname-osx)
+../../$(tarname-osx).xz: ../../$(tarname-osx)
 	xz -c < $< > $@
 
-dist-xz-osx: ../$(tarname-osx).xz
+dist-xz-osx: ../../$(tarname-osx).xz
 
-../$(exename): ../$(dirname-win)
+../../$(exename): ../../$(dirname-win)
 	makensis $</src/install/win/$(APPNAME).nsi
-	$(MV) $</src/install/win/$(exename) ../
+	$(MV) $</src/install/win/$(exename) ../../
 
-dist-win: ../$(exename)
+dist-win: ../../$(exename)
 
 dist: dist-bz2 dist-bz2-all dist-bz2-osx dist-win
 
-../$(tarname).bz2.torrent: ../$(tarname).bz2
+../../$(tarname).bz2.torrent: ../../$(tarname).bz2
 	rm -f $@
-	cd ../ &&\
+	cd ../../ &&\
 		mktorrent \
 		-a $(torrent-trackers-url) \
 		-w $(torrent-webseed-baseurl)/$(tarname).bz2 \
@@ -148,11 +148,11 @@ dist: dist-bz2 dist-bz2-all dist-bz2-osx dist-win
 		-c "Red Eclipse $(appversion) for Linux and BSD" \
 		$(tarname).bz2
 
-dist-torrent: ../$(tarname).bz2.torrent
+dist-torrent: ../../$(tarname).bz2.torrent
 
-../$(tarname-all).bz2.torrent: ../$(tarname-all).bz2
+../../$(tarname-all).bz2.torrent: ../../$(tarname-all).bz2
 	rm -f $@
-	cd ../ &&\
+	cd ../../ &&\
 		mktorrent \
 		-a $(torrent-trackers-url) \
 		-w $(torrent-webseed-baseurl)/$(tarname-all).bz2 \
@@ -160,11 +160,11 @@ dist-torrent: ../$(tarname).bz2.torrent
 		-c "$(appnamefull) $(appversion) for All Platforms" \
 		$(tarname-all).bz2
 
-dist-torrent-all: ../$(tarname-all).bz2.torrent
+dist-torrent-all: ../../$(tarname-all).bz2.torrent
 
-../$(tarname-osx).bz2.torrent: ../$(tarname-osx).bz2
+../../$(tarname-osx).bz2.torrent: ../../$(tarname-osx).bz2
 	rm -f $@
-	cd ../ &&\
+	cd ../../ &&\
 		mktorrent \
 		-a $(torrent-trackers-url) \
 		-w $(torrent-webseed-baseurl)/$(tarname-osx).bz2 \
@@ -172,11 +172,11 @@ dist-torrent-all: ../$(tarname-all).bz2.torrent
 		-c "$(appnamefull) $(appversion) for OSX" \
 		$(tarname-osx).bz2
 
-dist-torrent-osx: ../$(tarname-osx).bz2.torrent
+dist-torrent-osx: ../../$(tarname-osx).bz2.torrent
 
-../$(exename).torrent: ../$(exename)
+../../$(exename).torrent: ../../$(exename)
 	rm -f $@
-	cd ../ &&\
+	cd ../../ &&\
 		mktorrent \
 		-a $(torrent-trackers-url) \
 		-w $(torrent-webseed-baseurl)/$(exename) \
@@ -184,22 +184,22 @@ dist-torrent-osx: ../$(tarname-osx).bz2.torrent
 		-c "$(appnamefull) $(appversion) for Windows" \
 		$(exename)
 
-dist-torrent-win: ../$(exename).torrent
+dist-torrent-win: ../../$(exename).torrent
 
 dist-torrents: dist-torrent dist-torrent-all dist-torrent-osx dist-torrent-win
 
 dist-mostlyclean:
-	rm -rf ../$(dirname)
-	rm -rf ../$(dirname-win)
-	rm -f ../$(tarname)
-	rm -f ../$(tarname-all)
-	rm -f ../$(tarname-osx)
+	rm -rf ../../$(dirname)
+	rm -rf ../../$(dirname-win)
+	rm -f ../../$(tarname)
+	rm -f ../../$(tarname-all)
+	rm -f ../../$(tarname-osx)
 
 dist-clean: dist-mostlyclean
-	rm -f ../$(tarname)*
-	rm -f ../$(tarname-all)*
-	rm -f ../$(tarname-osx)*
-	rm -f ../$(exename)*
+	rm -f ../../$(tarname)*
+	rm -f ../../$(tarname-all)*
+	rm -f ../../$(tarname-osx)*
+	rm -f ../../$(exename)*
 
 ../doc/cube2font.txt: ../doc/man/cube2font.1
 	scripts/cube2font-txt $< $@
