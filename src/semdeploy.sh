aad6b22cfbd3dd814ad8e354693bde981e6f6e1a
qreeves <qreeves@gmail.com>
2015-03-24 20:57:32 +1000

    more semdeploy tweaks

--- src/semdeploy.sh
+++ src/semdeploy.sh
@@ -6,10 +6,20 @@ SEMABUILD_APT='DEBIAN_FRONTEND=noninteractive apt-get'
 
 sudo ${SEMABUILD_APT} update || exit 1
 sudo ${SEMABUILD_APT} -fy install build-essential nsis nsis-common || exit 1
-pushd src || exit 1
+
+mkdir -p "${HOME}/deploy/data" || exit 1
+( git archive stable | tar -x -C "${HOME}/deploy" ) || exit 1
+pushd "${SEMABUILD_PWD}/data" || exit 1
+( git archive master | tar -x -C "${HOME}/deploy/data" ) || exit 1
+popd || exit 1
+
+pushd "${HOME}/deploy/src" || exit 1
 make dist || exit 1
-popd
-mkdir releases || exit 1
+popd || exit 1
+
+pushd "${HOME}/deploy" || exit 1
+mkdir -p releases || exit 1
 mv -vf redeclipse_*.*_*.tar.bz2 releases/ || exit 1
 mv -vf redeclipse_*.*_*.exe releases/ || exit 1
 ${SEMABUILD_SCP} -r "releases" "${SEMABUILD_TARGET}" || exit 1
+popd || exit 1
