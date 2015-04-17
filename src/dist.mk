8a2c4e4f0820b400e25cc6d8c63aea5bc5567cf4
Quinton Reeves <qreeves@gmail.com>
2015-04-17 15:31:12 +1000

    more make dist-osx fixes

--- src/dist.mk
+++ src/dist.mk
@@ -61,12 +61,13 @@ dist-tar: ../$(tarname)
 	mkdir tmpdir-osx/$(dirname-osx)/Contents
 	mkdir tmpdir-osx/$(dirname-osx)/Contents/Files
 	# Use links with tar dereference to change directory paths
-	ln -s ../../$</config tmpdir-osx/$(dirname-osx)/Contents/Files/config
-	ln -s ../../$</data tmpdir-osx/$(dirname-osx)/Contents/Files/data
-	ln -s ../../$</doc tmpdir-osx/$(dirname-osx)/Contents/Files/doc
-	ln -s ../../$</src tmpdir-osx/$(dirname-osx)/Contents/Files/src
+	ln -s ../../$</config/ tmpdir-osx/$(dirname-osx)/Contents/Files/config
+	ln -s ../../$</data/ tmpdir-osx/$(dirname-osx)/Contents/Files/data
+	ln -s ../../$</doc/ tmpdir-osx/$(dirname-osx)/Contents/Files/doc
+	ln -s ../../$</src/ tmpdir-osx/$(dirname-osx)/Contents/Files/src
 	ln -s ../../$</readme.txt tmpdir-osx/$(dirname-osx)/Contents/Files/readme.txt
-	tar -hrf $@ -C tmpdir-osx $(dirname-osx)
+	tar \
+		-hrf $@ -C tmpdir-osx $(dirname-osx)
 	rm -rf tmpdir-osx/
 
 dist-tar-osx: ../$(tarname-osx)
@@ -136,7 +137,7 @@ dist-combined: ../$(tarname-combined).bz2
 dist-xz-combined: ../$(tarname-combined).xz
 
 ../$(exename): ../$(dirname-win)
-	makensis $</src/install/win/$(appname).nsi
+	makensis -V2 $</src/install/win/$(appname).nsi
 	$(MV) $</src/install/win/$(exename) ../
 
 dist-win: ../$(exename)
