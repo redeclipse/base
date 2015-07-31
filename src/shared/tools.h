b1bd4f9b7cae1d4f160da7ed9e8881405585289c
lsalzman <lsalzman@gmail.com>
2015-07-31 16:44:52 -0400

    add missing ulong type

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -11,6 +11,7 @@
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
+typedef unsigned long ulong;
 typedef signed long long int llong;
 typedef unsigned long long int ullong;
 
