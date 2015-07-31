292a43a4520574adaac64614b3686e74ef913e41
lsalzman <lsalzman@gmail.com>
2015-07-31 05:11:25 -0400

    cloudlayer fix

--- src/engine/rendersky.cpp
+++ src/engine/rendersky.cpp
@@ -211,7 +211,7 @@ void drawenvoverlay(int w, float height, int subdiv, float fade, float scale, Te
             gle::attrib(color, a);
         gle::attribf(p.x*w, p.y*w, z);
             gle::attribf(tx + p.x*tsz2, ty + p.y*tsz2);
-            gle::attrib(color, 0);
+            gle::attrib(color, 0.0f);
     }
     xtraverts += gle::end();
     gle::disable();
