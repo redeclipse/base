8f0f453a8b47ad191bb315f9f479a0a3889c1a69
Quinton Reeves <qreeves@gmail.com>
2016-01-20 21:14:06 +1000

    skew bar colour throb by fill amount

--- src/game/hud.cpp
+++ src/game/hud.cpp
@@ -2683,7 +2683,7 @@ namespace hud
         {
             const barstep &step = barsteps[id][i];
             vec colour(step.r, step.g, step.b);
-            if(throb >= 0 && i == int(roundf(3*throb))) switch(id)
+            if(throb >= 0 && i == int(roundf(3*throb*amt))) switch(id)
             {
                 case 1: colour = vec(barsteps[id][3].r, barsteps[id][3].g, barsteps[id][3].b); break;
                 case 2: colour = vec(1, 1, 1); break;
