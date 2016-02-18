d03ddb147aeccd678e5cc06a6f55619d050bdbe8
Quinton Reeves <qreeves@gmail.com>
2016-02-18 14:05:56 +1000

    fix server scripts to accept arguments

--- redeclipse_server.sh
+++ redeclipse_server.sh
@@ -2,4 +2,4 @@
 if [ -z "${REDECLIPSE_PATH+isset}" ]; then REDECLIPSE_PATH="$(cd "$(dirname "$0")" && pwd)"; fi
 REDECLIPSE_BINARY="redeclipse_server"
 export REDECLIPSE_BINARY
-REDECLIPSE_CALLED="true" . "${REDECLIPSE_PATH}/redeclipse.sh"
+REDECLIPSE_CALLED="true" . "${REDECLIPSE_PATH}/redeclipse.sh" $@
