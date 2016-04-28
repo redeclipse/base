b91e1388d281a4e7f26f8361650a90fcab873c9f
Quinton Reeves <qreeves@gmail.com>
2016-04-28 23:35:54 +1000

    v1.5.5 branding

--- src/engine/version.h
+++ src/engine/version.h
@@ -1,11 +1,11 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 5
-#define VERSION_PATCH 4
-#define VERSION_STRING "1.5.4"
+#define VERSION_PATCH 5
+#define VERSION_STRING "1.5.5"
 #define VERSION_NAME "Red Eclipse"
 #define VERSION_UNAME "redeclipse"
 #define VERSION_VNAME "REDECLIPSE"
-#define VERSION_RELEASE "Aurora Edition"
+#define VERSION_RELEASE "Elysium Edition"
 #define VERSION_URL "http://redeclipse.net/"
 #define VERSION_COPY "2010-2016"
 #define VERSION_DESC "A fun-filled new take on the first-person arena shooter."
