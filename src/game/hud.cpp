602c3ee64461799d700934b7c98d7bede4f29e95
Quinton Reeves <qreeves@gmail.com>
2016-05-25 11:36:43 +1000

    fixes #485

--- src/game/hud.cpp
+++ src/game/hud.cpp
@@ -567,8 +567,8 @@ namespace hud
 
     TVAR(IDF_PERSIST, modeeditingtex, "<grey>textures/modes/editing", 3);
     TVAR(IDF_PERSIST, modedeathmatchtex, "<grey>textures/modes/deathmatch", 3);
-    TVAR(IDF_PERSIST, modegladiator, "<grey>textures/modes/gladiator", 3);
-    TVAR(IDF_PERSIST, modeoldschool, "<grey>textures/modes/oldschool", 3);
+    TVAR(IDF_PERSIST, modegladiatortex, "<grey>textures/modes/gladiator", 3);
+    TVAR(IDF_PERSIST, modeoldschooltex, "<grey>textures/modes/oldschool", 3);
 
     TVAR(IDF_PERSIST, modecapturetex, "<grey>textures/modes/capture", 3);
     TVAR(IDF_PERSIST, modecapturequicktex, "<grey>textures/modes/capturequick", 3);
@@ -654,8 +654,8 @@ namespace hud
         } \
         else \
         { \
-            if(m_dm_gladiator(g, m)) ADDMODE(modegladiator) \
-            else if(m_dm_oldschool(g, m)) ADDMODE(modeoldschool) \
+            if(m_dm_gladiator(g, m)) ADDMODE(modegladiatortex) \
+            else if(m_dm_oldschool(g, m)) ADDMODE(modeoldschooltex) \
             else ADDMODE(modedeathmatchtex) \
         } \
     }
