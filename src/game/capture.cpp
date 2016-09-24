e7f396d309dca086eee27835dac3c9dab4ef9ef6
András Soltész <pocak100@gmail.com>
2016-09-24 21:31:26 +0200

    Fix protect-ctf.

--- src/game/capture.cpp
+++ src/game/capture.cpp
@@ -508,14 +508,25 @@ namespace capture
 
     void scoreaffinity(gameent *d, int relay, int goal, int score)
     {
-        if(!st.flags.inrange(relay) || !st.flags.inrange(goal)) return;
+        if(!st.flags.inrange(relay)) return;
+        float radius = enttype[AFFINITY].radius;
+        vec abovegoal, capturepos, returnpos;
         capturestate::flag &f = st.flags[relay];
-        capturestate::flag &g = st.flags[goal];
-        affinityeffect(goal, d->team, g.above, f.above, 3, "CAPTURED");
-        game::spawneffect(PART_SPARK, vec(g.spawnloc).add(vec(0, 0, enttype[AFFINITY].radius*0.45f)), enttype[AFFINITY].radius*0.25f, TEAM(g.team, colour), 1.5f);
-        game::spawneffect(PART_SPARK, vec(g.spawnloc).add(vec(0, 0, enttype[AFFINITY].radius*0.45f)), enttype[AFFINITY].radius*0.25f, 0xFFFFFF, 1.5f);
-        game::spawneffect(PART_SPARK, vec(f.spawnloc).add(vec(0, 0, enttype[AFFINITY].radius*0.45f)), enttype[AFFINITY].radius*0.25f, TEAM(f.team, colour), 1.5f);
-        game::spawneffect(PART_SPARK, vec(f.spawnloc).add(vec(0, 0, enttype[AFFINITY].radius*0.45f)), enttype[AFFINITY].radius*0.25f, 0xFFFFFF, 1.5f);
+        if (st.flags.inrange(goal))
+        {
+            capturestate::flag &g = st.flags[goal];
+            abovegoal = g.above;
+            capturepos = g.spawnloc;
+        }
+        else abovegoal = capturepos = f.pos(true); // m_ctf_protect
+        returnpos = vec(f.spawnloc);
+        returnpos.add(vec(0, 0, radius*0.45f));
+        capturepos.add(vec(0, 0, radius*0.45f));
+        affinityeffect(goal, d->team, abovegoal, f.above, 3, "CAPTURED");
+        game::spawneffect(PART_SPARK, capturepos, radius*0.25f, TEAM(d->team, colour), 1.5f);
+        game::spawneffect(PART_SPARK, capturepos, radius*0.25f, 0xFFFFFF, 1.5f);
+        game::spawneffect(PART_SPARK, returnpos, radius*0.25f, TEAM(f.team, colour), 1.5f);
+        game::spawneffect(PART_SPARK, returnpos, radius*0.25f, 0xFFFFFF, 1.5f);
         hud::teamscore(d->team).total = score;
         defformatstring(fteam, "%s", game::colourteam(f.team, "flagtex"));
         game::announcef(S_V_FLAGSCORE, CON_SELF, d, true, "\fa%s captured the %s flag for team %s (score: \fs\fc%d\fS, time taken: \fs\fc%s\fS)", game::colourname(d), fteam, game::colourteam(d->team), score, timestr(lastmillis-f.taketime, 1));
