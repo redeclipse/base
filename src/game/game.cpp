fab658649e8ded450286b323515a8a02a34de521
Quinton Reeves <qreeves@gmail.com>
2016-12-24 21:12:37 +1000

    some spectator camera tweaks

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2561,7 +2561,7 @@ namespace game
             cament *c = cameras[i];
             if(c->type == cament::PLAYER && (c->player || ((c->player = getclient(c->id)) != NULL)))
             {
-                if(!found && c->id == spectvfollowing && c->player->state != CS_SPECTATOR) found = true;
+                if(!found && c->id == spectvfollowing && allowspec(c->player, spectvdead, spectvfollowing)) found = true;
                 c->o = c->player->headpos();
                 if(forced && c->player == focus) cam = c;
             }
@@ -2582,18 +2582,18 @@ namespace game
                 cam->resetlast();
             }
         }
-        else loopk(spectvfollowing >= 0 ? 2 : 1)
+        else
         {
             int lastcn = cam->cn, millis = lasttvchg ? lastmillis-lasttvchg : 0;
             if(millis) amt = float(millis)/float(stvf(maxtime));
-            bool updated = camupdate(cam, amt, renew), override = !lasttvchg || millis >= stvf(mintime),
+            bool updated = camupdate(cam, amt, renew), ovr = !lasttvchg || millis >= stvf(mintime),
                  reset = (stvf(maxtime) && millis >= stvf(maxtime)) || !lasttvcam || lastmillis-lasttvcam >= stvf(time);
-            if(spectvfollowing >= 0 && !reset && !updated && !override)
+            if(spectvfollowing >= 0 && !updated && !reset)
             {
                 spectvfollowing = -1;
-                continue;
+                reset = true;
             }
-            if(reset || (!updated && override))
+            else if(reset || (!updated && ovr))
             {
                 loopv(cameras) if(cameras[i]->ignore) cameras[i]->ignore = false;
                 cam->ignore = true; // so we don't use the last one we just used
@@ -2650,7 +2650,6 @@ namespace game
                     }
                 }
             }
-            break;
         }
         bool chase = cam->player && (forced || spectvaiming(cam->player));
         if(!cam->player || chase)
