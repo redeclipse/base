2a9d11cdb2269029c60c1d8b3ca702fe169fdf37
Quinton Reeves <qreeves@gmail.com>
2017-01-26 21:41:21 +1000

    1.5.9-dev

--- src/engine/version.h
+++ src/engine/version.h
@@ -1,11 +1,11 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 5
-#define VERSION_PATCH 8
-#define VERSION_STRING "1.5.8"
+#define VERSION_PATCH 9
+#define VERSION_STRING "1.5.9"
 #define VERSION_NAME "Red Eclipse"
 #define VERSION_UNAME "redeclipse"
 #define VERSION_VNAME "REDECLIPSE"
-#define VERSION_RELEASE "Elysium Edition"
+#define VERSION_RELEASE "Development Build"
 #define VERSION_URL "http://redeclipse.net/"
 #define VERSION_COPY "2010-2017"
 #define VERSION_DESC "A fun-filled new take on the first-person arena shooter."
