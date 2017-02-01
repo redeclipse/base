51e78f951a9c8e0abb8ceb7798a27fcba32e1c07
Maxim Therrien <acerspyro@gmail.com>
2017-01-31 22:51:36 -0500

    Cleaned up join/part messages a bit

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1810,13 +1810,15 @@ namespace game
         if(!d) return;
         if(d->name[0] && client::showpresence >= (client::waiting(false) ? 2 : 1))
         {
+            defformatstring(formattedreason, " (%s)", disc_reasons[reason]);
+            defformatstring(ipaddr, " (%s)", d->hostname);
             if(d->actortype == A_PLAYER)
             {
                 int amt = client::otherclients(); // not including self to disclude this player
-                conoutft(CON_EVENT, "\fo%s (%s) left the game (%s) (%d %s)", colourname(d), d->hostname, reason >= 0 ? disc_reasons[reason] : "normal", amt, amt != 1 ? "players" : "player");
+                conoutft(CON_EVENT, "\fo%s%s left the game%s (%d %s)", colourname(d), (d->privilege&PRIV_TYPE) < G(iphostlock) ? ipaddr : "", reason >= 0 ? "" : formattedreason, amt, amt != 1 ? "players" : "player");
             }
             else if(d->actortype == A_BOT && ai::showaiinfo)
-                conoutft(CON_EVENT, "\fo%s was removed from the game (%s)", colourname(d), reason >= 0 ? disc_reasons[reason] : "normal");
+                conoutft(CON_EVENT, "\fo%s was removed from the game%s", colourname(d), reason >= 0 ? "" : formattedreason);
         }
         gameent *e = NULL;
         int numdyns = numdynents();
