b318f5c0b655e502835bb46af687b454753dd701
Quinton Reeves <qreeves@gmail.com>
2017-02-22 12:09:57 +1000

    backport memory leak fix

--- src/engine/rendertext.cpp
+++ src/engine/rendertext.cpp
@@ -659,6 +659,7 @@ textkey *findtextkey(const char *str)
     t->file = newstring(key);
     t->tex = textureload(t->file, 0, true, false);
     if(t->tex == notexture) t->tex = NULL;
+    textkeys.add(t);
     return t;
 }
 
@@ -682,6 +683,7 @@ tklookup *findtklookup(const char *str, int type)
     tklookup *t = new tklookup;
     t->name = newstring(str);
     t->type = type;
+    tklookups.add(t);
     return t;
 }
 
