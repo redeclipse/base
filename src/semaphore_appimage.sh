02645127ca8ddff51a1c7cf31656bd8da06c6020
Beha <shacknetisp@mail.com>
2017-11-03 00:21:15 -0400

    Use docker-external source, change appimage github token var name.

--- src/semaphore_appimage.sh
+++ src/semaphore_appimage.sh
@@ -2,6 +2,8 @@
 
 SEMABUILD_APT='DEBIAN_FRONTEND=noninteractive apt-get'
 
+RE_SOURCE="$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
+
 semabuild_appimage() {
     git clone https://github.com/red-eclipse/appimage-builder.git
 
@@ -13,10 +15,10 @@ semabuild_appimage() {
     export BUILD_SERVER=1
     export BUILD_CLIENT=1
     # Build the appimages.
-    bash build-with-docker.sh || exit 1
+    bash build-with-docker.sh "$RE_SOURCE" || exit 1
     # Release parameters.
-    export GITHUB_TOKEN=${APPIMAGE_GITHUB_TOKEN}
-    # Release the appimages
+    export GITHUB_TOKEN=${GITHUB_TOKEN}
+    # Release the appimages.
     bash github-release.sh || exit 1
     popd
     return 0
