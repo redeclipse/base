c7bc592ba7e6dfc6bc077ff7fb20a76a84218691
Quinton Reeves <qreeves@gmail.com>
2017-12-21 16:03:59 +1000

    v1.6.0 (Sunset Edition)

--- src/engine/version.h
+++ src/engine/version.h
@@ -1,11 +1,11 @@
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 5
-#define VERSION_PATCH 9
-#define VERSION_STRING "1.5.9"
+#define VERSION_MINOR 6
+#define VERSION_PATCH 0
+#define VERSION_STRING "1.6.0"
 #define VERSION_NAME "Red Eclipse"
 #define VERSION_UNAME "redeclipse"
 #define VERSION_VNAME "REDECLIPSE"
-#define VERSION_RELEASE "Elysium Edition"
+#define VERSION_RELEASE "Sunset Edition"
 #define VERSION_URL "http://redeclipse.net/"
 #define VERSION_COPY "2009-2017"
 #define VERSION_DESC "A fun-filled new take on the first-person arena shooter."
