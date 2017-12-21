4b8b0348f4b780cc71517a59b716d8f40e6d7fae
Quinton Reeves <qreeves@gmail.com>
2017-12-21 21:09:25 +1000

    fix dist-zip more

--- src/dist.mk
+++ src/dist.mk
@@ -160,7 +160,7 @@ dist-xz-combined: ../$(tarname-combined).xz
 	rm -rfv ../$(dirname-win)
 
 ../$(zipname): ../$(dirname-win)
-	cd .. && zip -vr "$(zipname)" $<
+	cd .. && zip -v9r "$(zipname)" "$(dirname-win)"
 	rm -rfv ../$(dirname-win)
 
 dist-win: ../$(exename)
