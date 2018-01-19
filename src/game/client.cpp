2077aa7aca8f55bd5812ebbfe898ff693ecabaca
Beha <shacknetisp@mail.com>
2018-01-19 04:28:08 -0500

    Use temp directory instead of maps directory for sent CRC-specified maps. (#684)
    * Keep temporary maps with CRC codes in temp/
    
    * Microsoft Windows exists, need to consider their path separator.
    
    * Remove temp prefix before testing and rebuilding filename for map to be sent.

--- src/game/client.cpp
+++ src/game/client.cpp
@@ -1602,7 +1602,9 @@ namespace client
                 len -= p.length();
                 if(filetype < 0 || filetype >= SENDMAP_MAX || len <= 0) break;
                 if(!*fname) copystring(fname, "maps/untitled");
-                defformatstring(ffile, strstr(fname, "maps/")==fname || strstr(fname, "maps\\")==fname ? "%s_0x%.8x" : "maps/%s_0x%.8x", fname, filecrc);
+                // Remove any temp/ prefix from file name before testing and rebuilding.
+                defformatstring(nfname, "%s", (strstr(fname, "temp/") == fname || strstr(fname, "temp\\") == fname) ? fname + 5 : fname);
+                defformatstring(ffile, strstr(nfname, "maps/")==nfname || strstr(nfname, "maps\\")==nfname ? "temp/%s_0x%.8x" : "temp/maps/%s_0x%.8x", nfname, filecrc);
                 defformatstring(ffext, "%s.%s", ffile, sendmaptypes[filetype]);
                 stream *f = openfile(ffext, "wb");
                 if(!f)
