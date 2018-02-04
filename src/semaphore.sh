87cfb7ba4baec9fd78e234aa980b2ad64dbea715
Quinton Reeves <qreeves@gmail.com>
2018-02-04 13:55:23 +1000

    attempt to fix i386 apt issues

--- src/semaphore.sh
+++ src/semaphore.sh
@@ -40,6 +40,15 @@ semabuild_archive() {
     return 0
 }
 
+semabuild_test() {
+    echo "testing ${BRANCH_NAME}..."
+    sudo ${SEMABUILD_APT} update || return 1
+    sudo ${SEMABUILD_APT} -fy install build-essential multiarch-support gcc-multilib g++-multilib zlib1g-dev libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev binutils-mingw-w64 g++-mingw-w64 || return 1
+    make PLATFORM=crossmingw64 PLATFORM_BIN=amd64 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/windows/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || return 1
+    make PLATFORM=linux64 PLATFORM_BIN=amd64 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/linux/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || return 1
+    return 0
+}
+
 semabuild_build() {
     echo "building ${BRANCH_NAME}..."
     sudo dpkg --add-architecture i386 || return 1
@@ -48,9 +57,12 @@ semabuild_build() {
     make PLATFORM=crossmingw64 PLATFORM_BIN=amd64 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/windows/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || return 1
     make PLATFORM=crossmingw32 PLATFORM_BIN=x86 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/windows/bin/x86 CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 -C src clean install || return 1
     make PLATFORM=linux64 PLATFORM_BIN=amd64 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/linux/bin/amd64 CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 -C src clean install || return 1
-    sudo apt-get purge -fy sbt || return 1
-    sudo apt-get -o Dpkg::Options::="--force-overwrite" -fy --no-install-recommends install gcc:i386 g++:i386 cpp:i386 g++-4.8:i386 gcc-4.8:i386 cpp-4.8:i386 binutils:i386 zlib1g-dev:i386 libsdl2-dev:i386 libsdl2-mixer-dev:i386 libsdl2-image-dev:i386 libpng12-dev:i386 || return 1
+    sudo ${SEMABUILD_APT} purge -fy sbt || return 1
+    sudo ${SEMABUILD_APT} -o Dpkg::Options::="--force-overwrite" -fy --no-install-recommends install gcc:i386 g++:i386 cpp:i386 g++-4.8:i386 gcc-4.8:i386 cpp-4.8:i386 binutils:i386 zlib1g-dev:i386 libsdl2-dev:i386 libsdl2-mixer-dev:i386 libsdl2-image-dev:i386 libpng12-dev:i386 || return 1
     make PLATFORM=linux32 PLATFORM_BIN=x86 PLATFORM_BUILD=${SEMAPHORE_BUILD_NUMBER} PLATFORM_BRANCH=${BRANCH_NAME} PLATFORM_REVISION=${REVISION} INSTDIR=${SEMABUILD_DIR}/linux/bin/x86 CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 -C src clean install || return 1
+    sudo ${SEMABUILD_APT} purge ".*:i386" || return 1
+    sudo dpkg --remove-architecture i386 || return 1
+    sudo ${SEMABUILD_APT} update || return 1
     return 0
 }
 
@@ -104,7 +116,7 @@ semabuild_process() {
     if [ "${BRANCH_NAME}" = master ] || [ "${BRANCH_NAME}" = stable ]; then
         semabuild_integrate || return 1
     else
-        semabuild_build || return 1
+        semabuild_test || return 1
     fi
     return 0
 }
