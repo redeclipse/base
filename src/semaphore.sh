815923850feaa03f55f286af97249e6e5eba5927
Quinton Reeves <qreeves@gmail.com>
2018-11-02 12:55:05 +1000

    fix depot build pt 12

--- src/semaphore.sh
+++ src/semaphore.sh
@@ -183,9 +183,12 @@ semabuild_steam() {
     tar --gzip --extract --verbose --overwrite --file="${SEMABUILD_DIR}/linux.tar.gz" --directory="${SEMABUILD_STEAM}/content"
     tar --gzip --extract --verbose --overwrite --file="${SEMABUILD_DIR}/macos.tar.gz" --directory="${SEMABUILD_STEAM}/content"
     pushd "${SEMABUILD_STEAM}" || return 1
-    steamcmd +login redeclipsebuild ${STEAM_TOKEN} +run_app_build_http app_build_967460.vdf +quit
+    curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+    chmod --verbose +x linux32/steamcmd || return 1
+    export LD_LIBRARY_PATH="${SEMABUILD_STEAM}/linux32:${LD_LIBRARY_PATH}"
+    ./linux32/steamcmd +login redeclipsebuild ${STEAM_TOKEN} +run_app_build_http app_build_967460.vdf +quit
     if [ $? -eq 42 ]; then
-        steamcmd +login redeclipsebuild ${STEAM_TOKEN} +run_app_build_http app_build_967460.vdf +quit
+        ./linux32/steamcmd +login redeclipsebuild ${STEAM_TOKEN} +run_app_build_http app_build_967460.vdf +quit
     fi
     popd || return 1
     return 0
@@ -197,7 +200,7 @@ semabuild_setup || exit 1
 #    semabuild_deploy || exit 1
     if [ "${BRANCH_NAME}" = master ]; then
         sudo ${SEMABUILD_APT} update || return 1
-        sudo ${SEMABUILD_APT} -fy install build-essential multiarch-support gcc-multilib g++-multilib zlib1g-dev libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev jq zsync steamcmd || exit 1
+        sudo ${SEMABUILD_APT} -fy install build-essential multiarch-support gcc-multilib g++-multilib zlib1g-dev libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev jq zsync || exit 1
         semabuild_steam || exit 1
     #    echo "building ${BRANCH_NAME} appimages..."
     #    pushd "${HOME}" || return 1
