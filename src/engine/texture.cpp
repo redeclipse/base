f895c9be83a5f3dd4ffde21566b00979c830b7c4
lsalzman <lsalzman@gmail.com>
2019-05-05 18:05:04 -0400

    envmapbb for boxed envmap radius

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -3071,6 +3071,7 @@ Texture *cubemapload(const char *name, bool mipit, bool msg, bool transient)
 }
 
 VAR(IDF_WORLD, envmapradius, 0, 128, 10000);
+VAR(IDF_WORLD, envmapbb, 0, 0, 1);
 VAR(IDF_PERSIST, aaenvmap, 0, 1, 1);
 
 struct envmap
@@ -3249,8 +3250,18 @@ ushort closestenvmap(const vec &o)
     loopv(envmaps)
     {
         envmap &em = envmaps[i];
-        float dist = em.o.dist(o);
-        if(dist < em.radius && dist < mindist)
+        float dist;
+        if(envmapbb)
+        {
+            if(!o.insidebb(vec(em.o).sub(em.radius), vec(em.o).add(em.radius))) continue; 
+            dist = em.o.dist(o);
+        }
+        else
+        {
+            dist = em.o.dist(o);
+            if(dist > em.radius) continue;
+        }
+        if(dist < mindist)
         {
             minemid = EMID_RESERVED + i;
             mindist = dist;
