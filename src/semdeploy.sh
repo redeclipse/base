dd7a22eade908bf4aef36969509b27512cb30fd3
Quinton Reeves <qreeves@gmail.com>
2019-12-20 12:00:28 +1000

    Comment out GitHub release stuff

--- src/semdeploy.sh
+++ src/semdeploy.sh
@@ -12,9 +12,9 @@ SEMABUILD_DIST="bz2 combined win zip mac"
 sudo ${SEMABUILD_APT} update || exit 1
 sudo ${SEMABUILD_APT} -fy install build-essential unzip zip nsis nsis-common mktorrent golang || exit 1
 
-export GOPATH="${HOME}/gofiles"
-go get "github.com/itchio/gothub"
-SEMABUILD_GHR="${GOPATH}/bin/gothub"
+#export GOPATH="${HOME}/gofiles"
+#go get "github.com/itchio/gothub"
+#SEMABUILD_GHR="${GOPATH}/bin/gothub"
 
 rm -rfv "${SEMABUILD_BUILD}"
 rm -rfv "${SEMABUILD_PWD}/data"
@@ -46,7 +46,7 @@ SEMABUILD_VERSION_MINOR=`sed -n 's/.define VERSION_MINOR \([0-9]*\)/\1/p' src/en
 SEMABUILD_VERSION_PATCH=`sed -n 's/.define VERSION_PATCH \([0-9]*\)/\1/p' src/engine/version.h`
 SEMABUILD_VERSION="${SEMABUILD_VERSION_MAJOR}.${SEMABUILD_VERSION_MINOR}.${SEMABUILD_VERSION_PATCH}"
 
-${SEMABUILD_GHR} release --user "redeclipse" --repo "base" --tag "v${SEMABUILD_VERSION}" --name "v${SEMABUILD_VERSION} (${SEMABUILD_RELEASE})" --description "${SEMABUILD_NAME} v${SEMABUILD_VERSION} (${SEMABUILD_RELEASE}) has been released!" --target "${BRANCH_NAME}" --draft
+#${SEMABUILD_GHR} release --user "redeclipse" --repo "base" --tag "v${SEMABUILD_VERSION}" --name "v${SEMABUILD_VERSION} (${SEMABUILD_RELEASE})" --description "${SEMABUILD_NAME} v${SEMABUILD_VERSION} (${SEMABUILD_RELEASE}) has been released!" --target "${BRANCH_NAME}" --draft
 
 for i in ${SEMABUILD_DIST}; do
     pushd "${SEMABUILD_BUILD}/src" || exit 1
@@ -78,7 +78,7 @@ for i in ${SEMABUILD_DIST}; do
         mv -vf "${q}" "releases/${q}" || exit 1
         shasum "releases/${q}" > "releases/${q}.shasum" || exit 1
         md5sum "releases/${q}" > "releases/${q}.md5sum" || exit 1
-        ${SEMABUILD_GHR} upload --user "redeclipse" --repo "base" --tag "v${SEMABUILD_VERSION}" --name "${q}" --file "releases/${q}" || exit 1
+        #${SEMABUILD_GHR} upload --user "redeclipse" --repo "base" --tag "v${SEMABUILD_VERSION}" --name "${q}" --file "releases/${q}" || exit 1
     done
     ${SEMABUILD_SCP} -r "releases" "${SEMABUILD_TARGET}" || exit 1
     rm -rfv releases || exit 1
