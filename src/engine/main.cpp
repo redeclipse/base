dcde7d66bc671ae73720435893149b425999cc14
Lee Salzman <lsalzman@gmail.com>
2020-11-12 00:04:17 -0500

    avoid redundant inputgrabs

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -546,7 +546,7 @@ void checkinput()
     {
         if(events.length()) event = events.remove(0);
 
-        if(focused && event.type!=SDL_WINDOWEVENT) { inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
+        if(focused && event.type!=SDL_WINDOWEVENT) { if(grabinput != (focused>0)) inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
 
         switch(event.type)
         {
@@ -644,7 +644,7 @@ void checkinput()
                 break;
         }
     }
-    if(focused) { inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
+    if(focused) { if(grabinput != (focused>0)) inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
     if(mousemoved)
     {
         warping = false;
