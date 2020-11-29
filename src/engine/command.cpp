c04b1d3ed5dcb20bd7cb19d2719887718d63e1a0
Lee Salzman <lsalzman@gmail.com>
2020-11-29 15:41:30 -0500

    set command

--- src/engine/command.cpp
+++ src/engine/command.cpp
@@ -1143,6 +1143,29 @@ void setsvarchecked(ident *id, const char *val)
     }
 }
 
+ICOMMAND(0, set, "rT", (ident *id, tagval *v),
+{
+    switch(id->type)
+    {
+        case ID_ALIAS:
+            if(id->index < MAXARGS) setarg(*id, *v); else setalias(*id, *v, (identflags&IDF_WORLD)!=0);
+            v->type = VAL_NULL;
+            break;
+        case ID_VAR:
+            setvarchecked(id, forceint(*v));
+            break;
+        case ID_FVAR:
+            setfvarchecked(id, forcefloat(*v));
+            break;
+        case ID_SVAR:
+            setsvarchecked(id, forcestr(*v));
+            break;
+        default:
+            debugcode("cannot redefine builtin %s with an alias", id->name);
+            break;
+    }
+});
+
 bool addcommand(const char *name, identfun fun, const char *args, int type, int flags, int level)
 {
     uint argmask = 0;
