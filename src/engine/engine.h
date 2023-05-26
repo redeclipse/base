f88da0e13206d72bba26648d666adc87c02c5095
Quinton Reeves <qreeves@gmail.com>
2023-05-26 14:59:15 +1000

    remove unused writecrosshairs

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -347,7 +347,6 @@ extern void zerofogcolor();
 extern void resetfogcolor();
 extern float calcfogdensity(float dist);
 extern float calcfogcull();
-extern void writecrosshairs(stream *f);
 
 namespace modelpreview
 {
