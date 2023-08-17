8b55b1983e6ce49267f61d770f467f10e582185e
Quinton Reeves <qreeves@gmail.com>
2023-08-18 04:48:04 +1000

    more semaphore blah

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -1,31 +1,15 @@
 #! /bin/bash
 
 SEMABUILD_GIT="${HOME}/${SEMAPHORE_GIT_DIR}"
-
-SEMABUILD_DEST="https://${GITHUB_TOKEN}:x-oauth-basic@github.com/redeclipse/deploy.git"
-SEMABUILD_BUILD="${HOME}/deploy"
-SEMABUILD_DIR="${SEMABUILD_BUILD}/${SEMAPHORE_GIT_BRANCH}"
+SEMABUILD_DIR="${HOME}/deploy"
 
 SEMABUILD_MODULES=`cat "${SEMABUILD_GIT}/.gitmodules" | grep '\[submodule "[^.]' | sed -e 's/^.submodule..//;s/..$//' | tr "\n" " " | sed -e 's/ $//'`
 SEMABUILD_ALLMODS="base ${SEMABUILD_MODULES}"
-SEMABUILD_DEPLOY="false"
 
 SEMABUILD_APT='DEBIAN_FRONTEND=noninteractive apt-get'
 
 semabuild_setup() {
     echo "setting up ${SEMAPHORE_GIT_BRANCH}.."
-
-    git config --global user.email "noreply@redeclipse.net" || return 1
-    git config --global user.name "Red Eclipse" || return 1
-    git config --global credential.helper store || return 1
-    echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" > "${HOME}/.git-credentials"
-
-    rm -rfv "${SEMABUILD_BUILD}" || return 1
-    rm -rfv "${SEMABUILD_GIT}/data" || return 1
-    pushd "${HOME}" || return 1
-    git clone --verbose --depth 1 "${SEMABUILD_DEST}" "${SEMABUILD_BUILD}" || return 1
-    ls -la "${SEMABUILD_BUILD}"
-    popd || return 1
     mkdir -pv "${SEMABUILD_DIR}" || return 1
 
     return 0
@@ -41,7 +25,6 @@ semabuild_archive() {
     tar -zcvf "${SEMABUILD_DIR}/linux.tar.gz" . || return 1
     popd || return 1
     rm -rfv "${SEMABUILD_DIR}/windows" "${SEMABUILD_DIR}/linux" || return 1
-    SEMABUILD_DEPLOY="true"
 
     return 0
 }
@@ -83,53 +66,11 @@ semabuild_build() {
 semabuild_integrate() {
     for i in ${SEMABUILD_ALLMODS}; do
         if [ "${i}" = "base" ]; then
-            SEMABUILD_MODDIR="${SEMABUILD_GIT}"
+            semabuild_build || return 1
+            semabuild_archive || return 1
         else
-            SEMABUILD_MODDIR="${SEMABUILD_GIT}/data/${i}"
-            echo "module ${i} updating.."
             git submodule update --init "data/${i}" || return 1
         fi
-        pushd "${SEMABUILD_MODDIR}" || return 1
-        echo "module ${i} processing.."
-        SEMABUILD_HASH=`git rev-parse HEAD`
-        if [ ! -e "${SEMABUILD_DIR}/${i}.txt" ]; then
-            echo "0" > "${SEMABUILD_DIR}/${i}.txt"
-        fi
-        SEMABUILD_LAST=`cat "${SEMABUILD_DIR}/${i}.txt"`
-        echo "module ${i} compare: ${SEMABUILD_LAST} -> ${SEMABUILD_HASH}"
-        if [ "${i}" = "base" ] && [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" != "true" ]; then
-            SEMABUILD_LAST="0"
-        fi
-        if [ -n "${SEMABUILD_HASH}" ] && [ "${SEMABUILD_HASH}" != "${SEMABUILD_LAST}" ]; then
-            echo "module ${i} updated, syncing.."
-            echo "${SEMABUILD_HASH}" > "${SEMABUILD_DIR}/${i}.txt"
-            if [ "${SEMABUILD_LAST}" = "0" ]; then
-                pushd "${SEMABUILD_BUILD}" || return 1
-                git add "${SEMAPHORE_GIT_BRANCH}/${i}.txt" || return 1
-                popd || return 1
-            fi
-            SEMABUILD_DEPLOY="true"
-            if [ "${i}" = "base" ]; then
-                echo "module ${i} checking for source modifications.."
-                SEMABUILD_CHANGES=""
-                SEMABUILD_BINS=`cat "${SEMABUILD_DIR}/bins.txt"`
-                if [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" != "true" ]; then
-                    SEMABUILD_CHANGES="<manual rebuild forced>"
-                else
-                    SEMABUILD_CHANGES=`git diff --name-only HEAD ${SEMABUILD_BINS} -- src | egrep '\.h$|\.c$|\.cpp$|Makefile$'`
-                fi
-                if [ -n "${SEMABUILD_CHANGES}" ]; then
-                    echo "module ${i} has modified source files:"
-                    echo "${SEMABUILD_CHANGES}"
-                    semabuild_build || return 1
-                    semabuild_archive || return 1
-                    echo "binary archive updated, syncing: ${SEMABUILD_HASH} -> ${SEMABUILD_BINS}"
-                    echo "${SEMABUILD_HASH}" > "${SEMABUILD_DIR}/bins.txt"
-                    echo "${SEMAPHORE_WORKFLOW_NUMBER}" > "${SEMABUILD_DIR}/build.txt"
-                fi
-            fi
-        fi
-        popd || return 1
     done
 
     return 0
@@ -145,16 +86,9 @@ semabuild_process() {
 }
 
 semabuild_deploy() {
-    echo "deploying ${SEMAPHORE_GIT_BRANCH}.."
-
-    echo "${SEMABUILD_ALLMODS}" > "${SEMABUILD_DIR}/mods.txt"
-    pushd "${SEMABUILD_BUILD}" || return 1
-    git status || return 1
-    git commit -a -m "Build ${SEMAPHORE_GIT_BRANCH}:${SEMAPHORE_WORKFLOW_NUMBER} from ${SEMAPHORE_GIT_SHA}" || return 1
-    git pull --rebase || return 1
-    git push -u origin master || return 1
-    popd || return 1
-
+    echo "deploying ${SEMAPHORE_GIT_BRANCH} from ${SEMABUILD_DIR}.."
+    ls -la "${SEMABUILD_DIR}"
+    artifact push workflow "${SEMABUILD_DIR}"
     return 0
 }
 
