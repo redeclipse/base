ca0cf306152e0db0bea7bb8e82b3152f215a5908
Quinton Reeves <qreeves@gmail.com>
2023-08-18 03:31:30 +1000

    more semaphore blah

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -2,7 +2,6 @@
 
 SEMABUILD_GIT="${HOME}/${SEMAPHORE_GIT_DIR}"
 SEMABUILD_BUILD="${HOME}/deploy"
-SEMABUILD_STEAM="${HOME}/depot"
 SEMABUILD_DIR="${SEMABUILD_BUILD}/${SEMAPHORE_GIT_BRANCH}"
 SEMABUILD_APT='DEBIAN_FRONTEND=noninteractive apt-get'
 SEMABUILD_DEST="git@github.com:redeclipse/deploy.git"
@@ -12,10 +11,7 @@ SEMABUILD_DEPLOY="false"
 
 semabuild_setup() {
     echo "setting up ${SEMAPHORE_GIT_BRANCH}.."
-    #git config --global user.email "noreply@redeclipse.net" || return 1
-    #git config --global user.name "Red Eclipse" || return 1
-    #git config --global credential.helper store || return 1
-    #echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" > "${HOME}/.git-credentials"
+
     rm -rfv "${SEMABUILD_BUILD}" || return 1
     rm -rfv "${SEMABUILD_GIT}/data" || return 1
     pushd "${HOME}" || return 1
@@ -23,17 +19,13 @@ semabuild_setup() {
     ls -la "${SEMABUILD_BUILD}"
     popd || return 1
     mkdir -pv "${SEMABUILD_DIR}" || return 1
+
     return 0
 }
 
 semabuild_archive() {
     echo "archiving ${SEMAPHORE_GIT_BRANCH}.."
-    # ensure updater and scripts updated at least if base fails
-    #cp -fv "redeclipse.bat" "${SEMABUILD_DIR}/windows/redeclipse.bat" || return 1
-    #cp -fv "redeclipse_server.bat" "${SEMABUILD_DIR}/windows/redeclipse_server.bat" || return 1
-    #cp -fv "redeclipse.sh" "${SEMABUILD_DIR}/linux/redeclipse.sh" || return 1
-    #cp -fv "redeclipse_server.sh" "${SEMABUILD_DIR}/linux/redeclipse_server.sh" || return 1
-    # create the archives
+
     pushd "${SEMABUILD_DIR}/windows" || return 1
     zip -r "${SEMABUILD_DIR}/windows.zip" . || return 1
     popd || return 1
@@ -42,27 +34,32 @@ semabuild_archive() {
     popd || return 1
     rm -rfv "${SEMABUILD_DIR}/windows" "${SEMABUILD_DIR}/linux" || return 1
     SEMABUILD_DEPLOY="true"
+
     return 0
 }
 
 semabuild_test() {
     echo "testing ${SEMAPHORE_GIT_BRANCH}.."
+
     sudo ${SEMABUILD_APT} update || return 1
     sudo ${SEMABUILD_APT} -fy install build-essential multiarch-support gcc-multilib g++-multilib zlib1g-dev libsdl2-dev libsndfile1-dev libalut-dev libopenal-dev libsdl2-image-dev libfreetype6-dev binutils-mingw-w64 g++-mingw-w64 || return 1
     sudo ${SEMABUILD_APT} clean || return 1
     make PLATFORM="crossmingw64" PLATFORM_BIN="amd64" PLATFORM_BUILD="${SEMAPHORE_WORKFLOW_NUMBER}" PLATFORM_BRANCH="${SEMAPHORE_GIT_BRANCH}" PLATFORM_REVISION="${SEMAPHORE_GIT_SHA}" WANT_DISCORD=1 WANT_STEAM=1 INSTDIR="${SEMABUILD_DIR}/windows/bin/amd64" CFLAGS="-m64 -O3 -fomit-frame-pointer -ffast-math" CXXFLAGS="-m64 -O3 -fomit-frame-pointer -ffast-math" LDFLAGS="-m64" -C src clean install || return 1
     make PLATFORM="linux64" PLATFORM_BIN="amd64" PLATFORM_BUILD="${SEMAPHORE_WORKFLOW_NUMBER}" PLATFORM_BRANCH="${SEMAPHORE_GIT_BRANCH}" PLATFORM_REVISION="${SEMAPHORE_GIT_SHA}" WANT_DISCORD=1 WANT_STEAM=1 INSTDIR="${SEMABUILD_DIR}/linux/bin/amd64" CFLAGS="-m64 -O3 -fomit-frame-pointer -ffast-math" CXXFLAGS="-m64 -O3 -fomit-frame-pointer -ffast-math" LDFLAGS="-m64" -C src clean install || return 1
+
     return 0
 }
 
 semabuild_build() {
     echo "building ${SEMAPHORE_GIT_BRANCH}.."
 
+    cache restore .buildindocker || mkdir -pv "${HOME}/.buildindocker" || return 1
+
     # remove now irrelevant cache
-    rm -rfv "${SEMAPHORE_CACHE_DIR}/sys_archives" 2> /dev/null
+    rm -rfv "${HOME}/.buildindocker/sys_archives"
 
     src/buildindocker.sh \
-        -c "${SEMAPHORE_CACHE_DIR}" \
+        -c "${HOME}/.buildindocker" \
         -g "$(pwd)" \
         -o "${SEMABUILD_DIR}" \
         -n "${SEMAPHORE_WORKFLOW_NUMBER}" \
@@ -70,6 +67,7 @@ semabuild_build() {
         -b "${SEMAPHORE_GIT_BRANCH}" || return 1
 
     sudo chmod -R a+rw "${SEMABUILD_DIR}" || return 1
+    cache store .buildindocker "${HOME}/.buildindocker" || return 1
 
     return 0
 }
@@ -91,7 +89,7 @@ semabuild_integrate() {
         fi
         SEMABUILD_LAST=`cat "${SEMABUILD_DIR}/${i}.txt"`
         echo "module ${i} compare: ${SEMABUILD_LAST} -> ${SEMABUILD_HASH}"
-        if [ "${i}" = "base" ] && [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" = "true" ]; then
+        if [ "${i}" = "base" ] && [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" != "true" ]; then
             SEMABUILD_LAST="0"
         fi
         if [ -n "${SEMABUILD_HASH}" ] && [ "${SEMABUILD_HASH}" != "${SEMABUILD_LAST}" ]; then
@@ -107,7 +105,7 @@ semabuild_integrate() {
                 echo "module ${i} checking for source modifications.."
                 SEMABUILD_CHANGES=""
                 SEMABUILD_BINS=`cat "${SEMABUILD_DIR}/bins.txt"`
-                if [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" = "true" ]; then
+                if [ "${SEMAPHORE_WORKFLOW_TRIGGERED_BY_HOOK}" != "true" ]; then
                     SEMABUILD_CHANGES="<manual rebuild forced>"
                 else
                     SEMABUILD_CHANGES=`git diff --name-only HEAD ${SEMABUILD_BINS} -- src | egrep '\.h$|\.c$|\.cpp$|Makefile$'`
@@ -125,6 +123,7 @@ semabuild_integrate() {
         fi
         popd || return 1
     done
+
     return 0
 }
 
@@ -139,6 +138,7 @@ semabuild_process() {
 
 semabuild_deploy() {
     echo "deploying ${SEMAPHORE_GIT_BRANCH}.."
+
     echo "${SEMABUILD_ALLMODS}" > "${SEMABUILD_DIR}/mods.txt"
     pushd "${SEMABUILD_BUILD}" || return 1
     git status || return 1
@@ -146,6 +146,7 @@ semabuild_deploy() {
     git pull --rebase || return 1
     git push -u origin master || return 1
     popd || return 1
+
     return 0
 }
 
