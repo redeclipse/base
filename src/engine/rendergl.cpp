e1d0b849276034b4e9e81f7fc70a1b82305c2d3b
Quinton Reeves <qreeves@gmail.com>
2024-01-26 11:23:45 +1000

    don't flicker visor in editmode

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -2585,7 +2585,7 @@ VAR(IDF_PERSIST|IDF_HEX, visorscanlinemixcolour, 0, 0xFFFFFF, 0xFFFFFF);
 FVAR(IDF_PERSIST, visorscanlinemixblend, 0.0, 0.67f, 1.0f);
 FVAR(IDF_PERSIST, visorscanlineblend, 0.0, 0.25f, 16.0f);
 FVAR(IDF_PERSIST, visornoiseblend, 0.0, 0.125f, 16.0f);
-FVAR(IDF_PERSIST, visorflickerblend, 0.0, 0.02f, 16.0f);
+FVAR(IDF_PERSIST, visorflickerblend, 0.0, 0.01f, 16.0f);
 
 void cleanupvisor();
 void setupvisor(int w, int h)
@@ -2727,7 +2727,7 @@ void gl_drawhud(bool noview = false)
         else SETSHADER(hudvisor);
 
         LOCALPARAMF(visorsize, visorw, visorh, 1.f/visorw, 1.f/visorh);
-        LOCALPARAMF(visorfx, visorscanlines, visorscanlineblend, visornoiseblend, visorflickerblend);
+        LOCALPARAMF(visorfx, visorscanlines, visorscanlineblend, visornoiseblend, editmode ? 0.f : visorflickerblend);
         vec4 color = vec4::fromcolor(visorscanlinemixcolour, visorscanlinemixblend);
         LOCALPARAMF(visorfxcol, color.r, color.g, color.b, color.a);
         LOCALPARAMF(time, lastmillis/1000.f);
