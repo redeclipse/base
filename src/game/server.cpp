05da74e2c30ce6494e77b065786bdff11b170c62
Quinton Reeves <qreeves@gmail.com>
2024-01-29 11:16:15 +1000

    prize sounds

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -796,7 +796,7 @@ namespace server
         }
     }
 
-    bool dropitems(clientinfo *ci, int flags = DROP_RESET)
+    bool dropitems(clientinfo *ci, int flags = DROP_RESET, int *realflags = NULL)
     {
         bool kamikaze = false;
         int ktype = A(ci->actortype, abilities)&(1<<A_A_KAMIKAZE) ? 3 : G(kamikaze);
@@ -842,6 +842,7 @@ namespace server
                 d.ent = ent;
                 d.ammo = 1; // one prize per customer
                 ci->dropped.add(d.ent, d.ammo);
+                if(realflags) *realflags |= HIT_PRIZE;
             }
         }
 
@@ -4883,7 +4884,7 @@ namespace server
             m->localtotaldeaths++;
             m->rewards[1] = 0;
 
-            dropitems(m, DROP_DEATH);
+            dropitems(m, DROP_DEATH, &realflags);
 
             static vector<int> dmglog;
             dmglog.setsize(0);
