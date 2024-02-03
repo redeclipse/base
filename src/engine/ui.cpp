ed379945022fe11b8aa43c62a908ca3971adf6f7
Quinton Reeves <qreeves@gmail.com>
2024-02-03 19:30:19 +1000

    be a little more generous to composites

--- src/engine/ui.cpp
+++ src/engine/ui.cpp
@@ -7124,7 +7124,7 @@ namespace UI
     #define COMPOSITESIZE (1<<9)
     extern void reloadcomp();
     VARF(IDF_PERSIST, compositesize, 1<<1, COMPOSITESIZE, 1<<12, reloadcomp());
-    VAR(IDF_PERSIST, compositeuprate, 0, 33, VAR_MAX); // limit updates to this ms
+    VAR(IDF_PERSIST, compositeuprate, 0, 10, VAR_MAX); // limit updates to this ms
     VAR(IDF_PERSIST, compositelimit, 0, 2, VAR_MAX); // limit updates to this count per cycle
     VAR(IDF_PERSIST, compositerewind, 0, 1, 1); // rewind if over time limit
 
