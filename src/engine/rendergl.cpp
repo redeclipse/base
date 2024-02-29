1ece336a24bb77004d76ee75cbbe44c59c1d0fee
Sławomir 'Q009' Błauciak <q009q009@gmail.com>
2024-02-29 12:07:12 +0100

    hud: hudquad doesn't need to be a macro

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -1771,25 +1771,26 @@ void screenquadoffset(float x, float y, float w, float h, float x2, float y2, fl
     screenquad();
 }
 
-#define HUDQUAD(x1, y1, x2, y2, sx1, sy1, sx2, sy2) { \
-    gle::defvertex(2); \
-    gle::deftexcoord0(); \
-    gle::begin(GL_TRIANGLE_STRIP); \
-    gle::attribf(x2, y1); gle::attribf(sx2, sy1); \
-    gle::attribf(x1, y1); gle::attribf(sx1, sy1); \
-    gle::attribf(x2, y2); gle::attribf(sx2, sy2); \
-    gle::attribf(x1, y2); gle::attribf(sx1, sy2); \
-    gle::end(); \
+void drawhudquad(float x1, float y1, float x2, float y2, float sx1, float sy1, float sx2, float sy2)
+{
+    gle::defvertex(2);
+    gle::deftexcoord0();
+    gle::begin(GL_TRIANGLE_STRIP);
+    gle::attribf(x2, y1); gle::attribf(sx2, sy1);
+    gle::attribf(x1, y1); gle::attribf(sx1, sy1);
+    gle::attribf(x2, y2); gle::attribf(sx2, sy2);
+    gle::attribf(x1, y2); gle::attribf(sx1, sy2);
+    gle::end();
 }
 
 void hudquad(float x, float y, float w, float h, float tx, float ty, float tw, float th)
 {
-    HUDQUAD(x, y, x+w, y+h, tx, ty, tx+tw, ty+th);
+    drawhudquad(x, y, x+w, y+h, tx, ty, tx+tw, ty+th);
 }
 
 void debugquad(float x, float y, float w, float h, float tx, float ty, float tw, float th)
 {
-    HUDQUAD(x, y, x+w, y+h, tx, ty+th, tx+tw, ty);
+    drawhudquad(x, y, x+w, y+h, tx, ty+th, tx+tw, ty);
 }
 
 VAR(0, fogoverlay, 0, 1, 2);
