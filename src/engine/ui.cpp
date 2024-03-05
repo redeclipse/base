0f65f5e4a7b267ae96785e4c1c3dbd56bc3f9396
Quinton Reeves <qreeves@gmail.com>
2024-03-06 04:05:16 +1000

    try nojittermatrix again

--- src/engine/ui.cpp
+++ src/engine/ui.cpp
@@ -1430,8 +1430,8 @@ namespace UI
             {
                 worldcalc();
 
-                //hudmatrix.muld(nojittermatrix, cammatrix);
-                hudmatrix = camprojmatrix;
+                hudmatrix.muld(nojittermatrix, cammatrix);
+                // hudmatrix = camprojmatrix;
                 hudmatrix.translate(pos);
                 hudmatrix.rotate_around_z(curyaw*RAD);
                 hudmatrix.rotate_around_x((curpitch - 90)*RAD);
