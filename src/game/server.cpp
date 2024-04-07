f67c0c0c352385fd0fdaa1a16e4555fa601be84b
Sławomir 'Q009' Błauciak <q009q009@gmail.com>
2024-04-07 18:29:39 +0200

    scripts: expose gamemillis

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -204,7 +204,8 @@ namespace server
         ~teamkill() {}
     };
 
-    int gamemode = G_EDITING, mutators = 0, gamemillis = 0, gamelimit = 0;
+    int gamemode = G_EDITING, mutators = 0, gamelimit = 0;
+    VAR(IDF_READONLY, gamemillis, 0, 0, INT_MAX);
 
     enum { WARN_CHAT = 0, WARN_TEAMKILL, WARN_MAX };
 
