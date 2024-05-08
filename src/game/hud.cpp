6677a0a7ae5bc766d0799e3bde0a5bc5fa766555
Quinton Reeves <qreeves@gmail.com>
2024-05-08 17:06:40 +1000

    visorcamvel fix

--- src/game/hud.cpp
+++ src/game/hud.cpp
@@ -102,9 +102,9 @@ namespace hud
     VAR(IDF_PERSIST|IDF_HEX, hitcrosshairtone, -CTONE_MAX, 0, 0xFFFFFF);
     VAR(IDF_PERSIST|IDF_HEX, clipstone, -CTONE_MAX, 0, 0xFFFFFF);
 
-    FVAR(IDF_PERSIST, visorcamvelx,     FVAR_MIN, 1.0f, FVAR_MAX);
-    FVAR(IDF_PERSIST, visorcamvely,     FVAR_MIN, 1.0f, FVAR_MAX);
-    FVAR(IDF_PERSIST, visorcamvelscale, 0.0f,     1.0f, FVAR_MAX);
+    FVAR(IDF_PERSIST, visorcamvelx, 0.0f, 1.0f, FVAR_MAX);
+    FVAR(IDF_PERSIST, visorcamvely, 0.0f, 1.0f, FVAR_MAX);
+    FVAR(IDF_PERSIST, visorcamvelscale, 0.0f, 1.0f, FVAR_MAX);
 
     VAR(IDF_PERSIST, showindicator, 0, 4, 4);
     FVAR(IDF_PERSIST, indicatorsize, 0, 0.03f, 1000);
@@ -1266,8 +1266,8 @@ namespace hud
 
         if(game::focus->isalive())
         {
-            if(visorcamvelx) x = game::focus->rotvel.x * visorcamvelx * visorcamvelscale;
-            if(visorcamvely) y = game::focus->rotvel.y * visorcamvely * visorcamvelscale;
+            if(visorcamvelx > 0.0f) x = game::focus->rotvel.x * visorcamvelx * visorcamvelscale;
+            if(visorcamvely > 0.0f) y = game::focus->rotvel.y * visorcamvely * visorcamvelscale;
         }
     }
 
